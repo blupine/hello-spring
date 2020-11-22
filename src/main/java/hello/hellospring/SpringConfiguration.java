771d0414bb6c1159f0ed2c8b297b4b07009e4e9a
blupine <skwint11@gmail.com>
2020-11-22 17:47:19 +0900

    [Impl] Member management using JDBC

--- src/main/java/hello/hellospring/SpringConfiguration.java
+++ src/main/java/hello/hellospring/SpringConfiguration.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.Repository.JdbcMemberRepository;
 import hello.hellospring.Repository.MemberRepository;
 import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfiguration {
+    //@Autowired private DataSource dataSource;
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,7 +27,7 @@ public class SpringConfiguration {
     // 설정파일로 Bean을 스프링에 등록했을 때 좋은 점?? - 아래 Repository의 수정이 필요할 때 주석과 같이 코드 한줄만 수정해서 바꿀 수 있음
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-        // return new DBMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
