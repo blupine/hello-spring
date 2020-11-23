e17d7faec2381f8a5e63de5a13332e73ceb43714
blupine <skwint11@gmail.com>
2020-11-23 22:30:08 +0900

    [Add] Integration test code

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,58 @@
+package hello.hellospring.service;
+
+import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional // Test 끝날 때 Transaction들 모두 rollback 해줌
+class MemberServiceIntegrationTest {
+
+    // 테스트 코드는 그냥 제일 편한 방법으로 구현... Field Injection 해도 괜춘...
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring2");
+        
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외_가입() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring2");
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
